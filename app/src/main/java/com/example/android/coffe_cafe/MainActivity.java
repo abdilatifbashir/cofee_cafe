eabe6e8b6676e731d21368828ac6e71fe5835f7c
abdilatifbashir <abdiladifbashir0000@gmail.com>
2018-02-11 09:30:37 +0300

    add display-price method nd increment-decrement funcionality

--- app/src/main/java/com/example/android/coffe_cafe/MainActivity.java
+++ app/src/main/java/com/example/android/coffe_cafe/MainActivity.java
@@ -7,25 +7,47 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import java.text.NumberFormat;
+
 
 public class MainActivity extends AppCompatActivity {
-    public static final String TAG=MainActivity.class.getSimpleName();
+    int quatity=2;
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-public void submitOrder(View view){
-display(7);
-    Log.d(TAG,"submitOrder:");
 
+    public void submitOrder(View view) {
+        display(2);
+        displayPrice(2 * 5);
+        Log.d(TAG, "submitOrder:");
+
+
+    }
+
+    private void display(int number) {
+        TextView numberOfCups = (TextView) findViewById(R.id.number_of_cups);
+        numberOfCups.setText("" + number);
+        Log.d(TAG, "submitOrder2:");
+    }
+
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    }
+
+    public void decrment(View view) {
+        quatity=quatity-1;
+        display(quatity);
+
+    }
+public void increment(View view){
+        quatity=quatity+1;
+        display(quatity);
 
 }
 
-private void display(int number){
-    TextView numberOfCups=(TextView) findViewById(R.id.number_of_cups);
-    numberOfCups.setText(""+number);
-    Log.d(TAG,"submitOrder2:");
-}
 }
